49a006788fff59f57617ab1b56b00782e0115678
Thongpak Pongsilathong <thongpak21@gmail.com>
2019-05-12 12:57:58 +0700

    add dhttask cpp

--- src/DHTTask.cpp
+++ src/DHTTask.cpp
@@ -1,14 +1,63 @@
 
 #include <Arduino.h>
 #include <Scheduler.h>
-class MemTask : public Task
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHTTYPE DHT11
+
+#define DHTPIN D3
+DHT dht(DHTPIN, DHTTYPE);
+
+class DHTTask : public Task
 {
-public:
+  void DHTRunning()
+  {
+    delay(2000);
+
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    float h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    float t = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    float f = dht.readTemperature(true);
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t) || isnan(f))
+    {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    }
+
+    // Compute heat index in Fahrenheit (the default)
+    float hif = dht.computeHeatIndex(f, h);
+    // Compute heat index in Celsius (isFahreheit = false)
+    float hic = dht.computeHeatIndex(t, h, false);
+
+    Serial.print(F("Humidity: "));
+    Serial.print(h);
+    Serial.print(F("%  Temperature: "));
+    Serial.print(t);
+    Serial.print(F("°C "));
+    Serial.print(f);
+    Serial.print(F("°F  Heat index: "));
+    Serial.print(hic);
+    Serial.print(F("°C "));
+    Serial.print(hif);
+    Serial.println(F("°F"));
+  }
+
+protected:
+  void setup()
+  {
+    dht.begin();
+  }
   void loop()
   {
-    Serial.print("Free Heap: ");
-    Serial.print(ESP.getFreeHeap());
-    Serial.println(" bytes");
-    delay(10000);
+    DHTRunning();
   }
-} mem_task;
+
+private:
+  uint8_t state;
+} dht_task;
